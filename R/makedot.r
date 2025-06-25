0007897606fcf60c23787ec66cc679a1fc9b1350
harrelfe <fh@fharrell.com>
2025-06-25 09:05:29 +0200

    makedot: added format customization enhancements

--- R/makedot.r
+++ R/makedot.r
@@ -13,12 +13,18 @@
 ##'
 ##' If a node label contains `\\n`, all `\\n` are replaced with `\n` and ordinary word
 ##' wrapping is suppressed.  This is how users take control of line splitting.
+##' If a node label ends in `//any text`, the text in `any text` will be copied 
+##' to inside the node definition after the label is closed with double quote and
+##' after a space is added.  This allows node colors and other attributes to be changed.
+##'
+##' Preceed single quotes in node labels with a single backslash.  Preceed double quotes with double backslashes.
 ##' 
 ##' The function uses [knitr::knit_expand()] to apply variable insertions before the diagram is rendered by Quarto.  See [this](https://hbiostat.org/rflow/doverview.html#sec-doverview-filter/) for an example.  ##' @title makedot
 ##' @param .object. character string or vector of plain text plus possible $\LaTeX$ math delimited by single dollar signs.  An empty initial line is ignore, so the user need not worry about having an initial quote mark on a line by itself.
 ##' @param direction direction of the flow chart, default is top-down
 ##' @param shape node shape
 ##' @param fontsize text font size
+##' @param nodesetup extra node setup to include after `direction`
 ##' @param width text width for word-wrapping
 ##' @param ... name=value pairs that makes values replace `{{name}}` elements in the markup
 ##' @param file name of file to hold `graphviz` markup after variable insertions.  Run this in Quarto using a chunk to looks like what is below, which was for `file='graphviz.dot'`.
@@ -40,7 +46,7 @@
 ##' x <- '
 ##' A:Some string of text for {{jj}}
 ##' B:Another string of test, this one being longer and needing breaking
-##' C:A third string defining a node
+##' C:A third string defining a node//style="filled,rounded", fillcolor=lightblue
 ##' A -> B
 ##' B -> C [label="some text along connecting arc"]
 ##' C -> A'
@@ -48,7 +54,7 @@
 ##' makedot(x, jj='tiger', onlyprint=TRUE)
 
 makedot <- function(.object., ..., direction=c('TD', 'LR'),
-  shape='box', fontsize=24, file, width=30, onlyprint=FALSE) {
+  shape='box', fontsize=24, nodesetup=NULL, file, width=30, onlyprint=FALSE) {
   direction <- match.arg(direction)
 
   x <- .object.
@@ -62,13 +68,22 @@ makedot <- function(.object., ..., direction=c('TD', 'LR'),
       w <- sub('^.*?:', '', y)
       # If line has forced line breaks with \\n, change these to real
       # \n link breaks and don't do any automatic word wrapping
-      w <- if(grepl('\\n', w, fixed=TRUE)) gsub('\\n', '\n', w, fixed=TRUE) 
-      else sapply(strwrap(w, width, simplify=FALSE), paste, collapse='\n')
-      x[i] <- paste0(n, '[label="', w, '"]')
+      newline_manual <- grepl('\\n', w, fixed=TRUE)
+      if(newline_manual) w <- gsub('\\n', '\n', w, fixed=TRUE)
+      extranode_info <- ''
+      if(grepl('.*//.*', w)) {
+        extranode_info <- paste0(', ', sub('.*//', '', w))
+        w <- sub('//.*', '', w)
+      }
+      if(! newline_manual)
+        w <- sapply(strwrap(w, width, simplify=FALSE), paste, collapse='\n')
+      x[i] <- paste0(n, '[label="', w, '"', extranode_info, ']')
     }
   }
 
-  head <- paste0('digraph {rankdir =', direction, ';\nnode [shape=', shape,
+  head <- paste0('digraph {rankdir =', direction, ';',
+            if(length(nodesetup)) paste0('\n', nodesetup, ';'),
+            '\nnode [shape=', shape,
                  ', fontsize=', fontsize, '];')
   x <- c(head, x)
 
